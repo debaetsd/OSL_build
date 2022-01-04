eb76f2b46441673a2bcb88be573476ef8023b5a4
Dieter De Baets <dieter.baets@unity3d.com>
2022-01-04 21:36:44 +0100

    seems to compile
    Signed-off-by: Dieter De Baets <dieter.baets@unity3d.com>

--- Build.bat
+++ Build.bat
@@ -15,7 +15,7 @@ set compiler="Visual Studio 16 2019"
 set arch="x64"
 
 rem Boost is special (who would have guessed)
-if not exist boost git clone -j8 --recurse-submodules --depth 1 --branch boost-1.73.0 https://github.com/boostorg/boost.git boost
+if not exist boost git clone -j8 --recurse-submodules --depth 1 --branch boost-1.77.0 https://github.com/boostorg/boost.git boost
 pushd boost
 if not exist builddir mkdir builddir
 call bootstrap.bat
@@ -26,19 +26,17 @@ set libDir=%outputDir%/lib
 set binDir=%outputDir%/bin
 robocopy %libDir% %binDir% "Boost_*.dll" /NFL /NDL /NJH /NJS /nc /ns /np
  
- 
 set libs=zlib
 set libs=%libs%;winflexbison
 set libs=%libs%;pugixml
 set libs=%libs%;LLVM
-
 set libs=%libs%;libjpeg-turbo
 set libs=%libs%;libtiff
 set libs=%libs%;openexr
+set libs=%libs%;pybind11
 set libs=%libs%;oiio
-
 set libs=%libs%;OpenShadingLanguage
-set libs=%libs%;MaterialX
+rem set libs=%libs%;MaterialX
 
 for %%f in (%libs%) do (
 	set dep_name=%%~nf
